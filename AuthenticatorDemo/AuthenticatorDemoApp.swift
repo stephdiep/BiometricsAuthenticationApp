7eaa1bbc3a970c0fe341fb0575499c70a2b1ccc7
stephdiep <stephanie.diep002@gmail.com>
2021-12-15 12:55:21 -0500

    Initial Commit

---
+++ AuthenticatorDemo/AuthenticatorDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthenticatorDemoApp.swift
+//  AuthenticatorDemo
+//
+//  Created by Stephanie Diep on 2021-12-15.
+//
+
+import SwiftUI
+
+@main
+struct AuthenticatorDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
